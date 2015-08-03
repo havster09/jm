3e9e297c1321cc8cd6543510a3bf0e5ce93c525c
unknown <havster09@gmail.com>
2015-08-03 19:38:22 +1000

    anim

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -21,6 +21,21 @@ angular.module('jmApp')
             var btn = $compile('<btn val="'+v+'"></btn>')($scope);
             angular.element(document.getElementById('holder')).append(btn);
         }
+        $scope.items = [];
+
+        $scope.addItems = function(){
+            for(var i = 10; i > 1; i--){
+                $scope.items.push({'title':'item'+i});
+            }
+        }
+        $scope.clearItems = function(){
+            $scope.items = [];
+        }
+        $scope.removeItem = function(index){
+            $scope.items.splice(index,1);
+        }
+
+
     }])
     .directive("btn",function(){
         return{
